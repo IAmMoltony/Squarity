c53f1aa2850a6411f985d7ee0b02489191eca034
IAmMoltony <koronavirusnyj@gmail.com>
2023-09-21 17:42:00 +0600

    Remove reduntant virtual from BasicEnemy declaration

--- include/BasicEnemy.h
+++ include/BasicEnemy.h
@@ -8,14 +8,14 @@ class BasicEnemy : public Enemy
 public:
     BasicEnemy(int x, int y);
 
-    virtual void Draw(SDL_Renderer *rend) override;
+    void Draw(SDL_Renderer *rend) override;
     void Update(Trail::List &trails) override;
 
     int GetWidth(void) const override;
     int GetHeight(void) const override;
     Type GetType(void) const override;
     SDL_Rect GetRect(void) const override;
-    virtual SDL_Color GetColor(void) const override;
+    SDL_Color GetColor(void) const override;
 
 private:
     virtual void initVelocity(void);
