82b338a51ad2ec9d8ba6f64a5c1a8325a7b77e1e
IAmMoltony <koronavirusnyj@gmail.com>
2023-09-21 15:53:48 +0600

    Actually add fast enemy files

---
+++ src/FastEnemy.cpp
@@ -0,0 +1,26 @@
+#include "FastEnemy.h"
+
+FastEnemy::FastEnemy(int x, int y) : BasicEnemy(x, y)
+{
+    initVelocity();
+}
+
+void FastEnemy::initVelocity(void)
+{
+    velX = (std::rand() % 2) ? 3 : -3;
+    velY = (std::rand() % 2) ? 7 : -7;
+}
+
+FastEnemy::~FastEnemy()
+{
+}
+
+SDL_Color FastEnemy::GetColor(void) const
+{
+    SDL_Color color;
+    color.r = 0;
+    color.g = 255;
+    color.b = 255;
+    color.a = 255;
+    return color;
+}
